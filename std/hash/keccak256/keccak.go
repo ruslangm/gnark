f09fb1176cfce3f456cbc40728602a6d8098beac
Ruslan <ruslan.gmz@gmail.com>
2022-12-28 14:53:10 +0400

    some modifications

--- std/hash/keccak256/keccak.go
+++ std/hash/keccak256/keccak.go
@@ -80,16 +80,21 @@ func (h *Keccak256) flush() {
 		fmt.Printf("Updated value of h.a[%v] with b=%v\n", i, piUint64)
 		b = b[8:]
 	}
-	h.keccakf()
+	h.a = h.keccakf()
 	h.len = 0
 }
 
-func (h *Keccak256) keccakf() {
+func (h *Keccak256) keccakf() [25]keccakf2.Xuint64 {
 	in := [25]frontend.Variable{}
 	for i := range h.a {
 		in[i] = h.uapi64.FromUint64(h.a[i])
 	}
-	keccakf2.Permute(h.api, in)
+	keccakf := keccakf2.Permute(h.api, in)
+	res := [25]keccakf2.Xuint64{}
+	for i := range keccakf {
+		res[i] = h.uapi64.AsUint64(keccakf[i])
+	}
+	return res
 }
 
 func (h *Keccak256) Sum(data ...frontend.Variable) []frontend.Variable {
