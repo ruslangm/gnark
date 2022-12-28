70fefc62e4f75b6e14c015457f216a042e391cdd
Ruslan <ruslan.gmz@gmail.com>
2022-12-28 19:09:02 +0400

    small refactor

--- std/permutation/keccakf/uint8api_test.go
+++ std/permutation/keccakf/uint8api_test.go
@@ -1,6 +1,8 @@
 package keccakf
 
 import (
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/backend"
 	"testing"
 
 	"github.com/consensys/gnark/frontend"
@@ -16,6 +18,11 @@ type andUint8Circ struct {
 type orUint8Circ struct {
 	In1 frontend.Variable
 	In2 frontend.Variable
+	Out frontend.Variable
+}
+
+type uint8ToUint64Circ struct {
+	In  []frontend.Variable
 	Out frontend.Variable
 }
 
@@ -39,6 +46,21 @@ func (c *orUint8Circ) Define(api frontend.API) error {
 	return nil
 }
 
+func (c uint8ToUint64Circ) Define(api frontend.API) error {
+	uapi := NewUint8API(api)
+	uapi64 := NewUint64API(api)
+
+	in := make([]Xuint8, len(c.In))
+	for i, v := range c.In {
+		in[i] = uapi.AsUint8(v)
+	}
+	res := DecodeToXuint64(in)
+
+	out := uapi64.AsUint64(c.Out)
+	uapi64.assertEq(out, res)
+	return nil
+}
+
 func TestAndOperation(t *testing.T) {
 	assert := test.NewAssert(t)
 	assert.ProverSucceeded(&andUint8Circ{In1: 2, In2: 6, Out: 2}, &andUint8Circ{In1: 2, In2: 6, Out: 2})
@@ -47,4 +69,18 @@ func TestAndOperation(t *testing.T) {
 func TestOrOperation(t *testing.T) {
 	assert := test.NewAssert(t)
 	assert.ProverSucceeded(&orUint8Circ{In1: 2, In2: 6, Out: 6}, &orUint8Circ{In1: 2, In2: 6, Out: 6})
+}
+
+func TestDecodeUint8ToUint64Operation(t *testing.T) {
+	assert := test.NewAssert(t)
+	in := []frontend.Variable{8, 7, 6, 5, 4, 3, 2, 1}
+
+	var circuit, witness uint8ToUint64Circ
+	witness.In = in
+	witness.Out = 15
+	assert.SolvingSucceeded(
+		&circuit,
+		&witness,
+		test.WithBackends(backend.GROTH16),
+		test.WithCurves(ecc.BN254))
 }
